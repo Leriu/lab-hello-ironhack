fcae90dbf506b7f0202c0b8768ee6533fe627470
Leriu <cf_uriel@hotmail.com>
2018-08-24 11:41:20 -0500

    done

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component} from "react";
+import "./App.css";
+import menu from './menu-top.svg'
+
+class App extends Component{
+render(){
+    return(
+        <div className="App">
+        <img src={menu}></img>
+
+
+        </div>
+        );
+    }
+}
+export default App;
